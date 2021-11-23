1496d2e532f16658823dfa91a46f71cb22da8407
RodrigoMoreno <rroderickk@gmail.com>
2021-11-23 03:07:07 -0500

    Provocando un error de un output en monohilo.js

---
+++ conceptos/monohilo.js
@@ -0,0 +1,17 @@
+console.log("Hola mundo");
+
+let i = 0;
+setInterval(function() {
+    console.log(i);
+    i++;
+    
+    if (i === 5) {
+        console.log('Foramos Error');
+        var a = 3 + z;
+    }
+}, 1000);
+
+
+
+
+
