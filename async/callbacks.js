bb5a90e810ead4bfc77ae459c58cda79d2bb5e92
RodrigoMoreno <rroderickk@gmail.com>
2021-11-25 10:22:33 -0500

    preguntas to platzi

--- async/callbacks.js
+++ async/callbacks.js
@@ -17,31 +17,43 @@ function soyAsincronaDos(a, b) {
   let multiplicoPorDos = trescuartos * 2;
   let dividoPorDos = trescuartos / 2;
   console.log(
-    multiplicoPorDos * a + " elemento inverso asociado a la multiplicacion"
+    [multiplicoPorDos * a] + " elemento inverso asociado a la multiplicacion"
   );
   console.log(
     dividoPorDos * b +
       " :b dividir es multiplicar por el inverso multipilcativo de b, \nelemento inverso asociado a la multiplicacion es a por uno partido b"
   );
-  function chao(rodri, otroCallback) {
-    setTimeout(function (otrocallback) {
-      console.log(
-        "[-] Y adios " +
-          [(rodri = "platzi")] +
-          " buena noche! recuerda mi nombre soy" +
-          cortana
-      );
-    }, 1000);
-  }
+
   console.log("Entonces es igual a " + (a * 1) / b);
   setTimeout(function (miCallback = soyAsincrona("wtf")) {
     console.log("mientras estoy siendo asincrona " + [miCallback]);
     chao("rodri");
   }, 1000);
+  function chao(rodri, otrocallback) {
+    setTimeout(function () {
+      console.log(
+        "[-] Y adios " +
+          [(rodri = "platzi")] +
+          " buena noche! recuerda mi nombre soy" +
+          cortana +
+          [otrocallback]
+      );
+    }, 2500);
+  }
 }
 
 console.log("[+] Iniciando proceso...");
+
 soyAsincronaDos(8, 8);
+
+function test1(micall) {
+  setTimeout(function () {
+    console.log(cortana + " aparece salvajemente! ");
+    /*micall(); /*por que si escribo el codigo del profe rompe el codigo diciendo que micall no es una funcion? actualizaron node?*/ 
+  }, 1);
+}
+test1();
+
 console.log("[-] Terminando proceso...");
 /* Estamos ocupados o estamos siendo productivos?
 ENFOCATE */
