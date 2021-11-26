00fe686a2189a6ad8069f21bfaa38c0e3c3edaee
RodrigoMoreno <rroderickk@gmail.com>
2021-11-26 10:59:51 -0500

    promises

--- async/callbackHell.js
+++ async/callbackHell.js
@@ -20,35 +20,55 @@ function hola(nombre, miCallback) {
   setTimeout(function () {
     console.log("Hola " + nombre);
     miCallback(nombre);
-  }, 1000);
+  }, 10);
 }
 
 function hablar(callBackHablar) {
   setTimeout(function () {
-    console.log(
-      "Como estas? sabias que un gugol es 10 elevado a 100?"
-    );
+    console.log("Como estas? sabias que un gugol es 10 elevado a 100?");
     callBackHablar();
-  }, 1100);
+  }, 11);
 }
 
 function adios(nombre, otroCallback) {
   setTimeout(function () {
-    console.log(
-      "Eso " + nombre + " es mi velocidad\ny poder de procesamiento"
-    );
+    console.log("Eso " + nombre + " es mi velocidad\ny poder de procesamiento");
     otroCallback(nombre);
-  }, 1800);
+  }, 18);
+}
+
+function conversacion(nombre, veces, callback) {
+  if (veces >= 0) {
+    hablar(function () {
+      conversacion(nombre, --veces, callback);
+    });
+  } else {
+    adios(nombre, callback);
+  }
 }
 
 console.log("[+] Iniciando proceso...");
 hola("Platzi", function (nombre) {
-  hablar(function () {
-    adios(nombre, function () {
-      console.log("[-] Adios " + nombre + " Terminando proceso...");
-    });
+  conversacion(nombre, 4, function () {
+    console.log("[-] Proceso termiando...");
   });
 });
 
+/*  ESTO ES LO QUE SE LLAMA CALLBACKSHELLS  */
+/* console.log("[+] Iniciando proceso...");
+hola("Platzi", function (nombre) {
+  hablar(function () {
+    hablar(function () {
+      hablar(function () {
+        hablar(function () {
+          adios(nombre, function () {
+            console.log("[-] Adios " + nombre + " Terminando proceso...");
+          });
+        });
+      });
+    });
+  });
+});
+ */
 /* Estamos ocupados o estamos siendo productivos?
 ENFOCATE */
