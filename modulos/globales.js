dca3a9ef4c1010f5f01f052747f09ee37a0c0418
RodrigoMoreno <rroderickk@gmail.com>
2021-12-02 12:25:29 -0500

    clase globales terminada globales.js

--- modulos/globales.js
+++ modulos/globales.js
@@ -8,7 +8,7 @@ return new Promise(function (resolve, reject) {
 });
 }
 
-async function banner(nombre,Jarvis) {
+async function banner(nombre,Jarvis,intb) {
 return new Promise(function (resolve, reject) {
     setTimeout(function () {
         let Wellcome = "Wellcome Sir,"
@@ -16,26 +16,26 @@ return new Promise(function (resolve, reject) {
         console.log(`${Jarvis} ${nombre}`);
         console.log("###########======########======##########");
         resolve(nombre);
-    }, 30);
+    }, intb);
 });
 }
 
-async function codigo(nombre) {
+async function codigo(nombre,intc) {
     return new Promise(function (resolve, reject) {
         setTimeout(function () {
             console.log(nombre);
             resolve();
-        }, 60);
+        }, intc);
     });
 }
 
-async function rodri(nombreRodri,pregunta) {
+async function rodri(nombreRodri,pregunta,intr) {
     return new Promise(function (resolve, reject) {
         setTimeout(function () {
             nombreRodri = "Rodri:"
             console.log(`${nombreRodri} ${pregunta}`);
             resolve();
-        }, 60);
+        }, intr);
     });
 }
 
@@ -52,31 +52,62 @@ async function ejemplo1(nombre) {
         }, 1000); 
         console.log(nombre);
                     resolve();
-                }, 1000);
+                }, 300);
             });
 }
 
 let nombre = await banner("Rodri","Jarvis: Wellcome Sir");
 let nombre2 = await nombreP("Platzi");
+
 await banner("Platzi","Jarvis: Wellcome Sir");
-await banner("Esto es un codigo que puedes usar ","Jarvis: ");
-await codigo("ESTO ES UN PEDAZO DE CODIGO:\n let i = 0;  \n let intervalo = setinterval(function () {\n\t console.log(hola mundoooooo);\n\t i++ if (i === 3) {\n\t clearinterval(intervalo);\n\t }\n }, 100); ");
-await rodri("R","y para que sirve tal cosAAAA? ")
+await banner("Esto es un codigo que puedes usar: ","Jarvis: ");
+await codigo("\n let i = 0;  \n let intervalo = setInterval(function () {\n\t console.log(\"hola mundoooooo\");\n\t i++ if (i === 3) {\n\t clearInterval(intervalo);\n\t }\n }, 100); \n");
+await rodri("R","y para que sirve tal cosAAAA? ",10)
 await ejemplo1("Jarvis: Imprimira esta secuencia y cortara al imprimirlo i veces checkea: ");
 
+await rodri("R","Excelente ejemplo, que mas tienes? ",3100)
 
-// console.log(`Terminando proceso ${nombre}, ${nombre2}`)
+await banner("Si ejecutas esta linea\n console.log(global);\n Ejecutara lo siguiente: ","Jarvis: ",1000);
+console.log(global);
+await banner("Puedes observar propiedades del modulo global xD","Jarvis: ",1000);
 
+await banner("Si ejecutas esta linea\n console.log(setInterval);\n Ejecutara lo siguiente: ","Jarvis: ",1000);
+console.log(setInterval);
+await banner("Puedes observar propiedades del modulo setInterval xD","Jarvis: ",1000);
+
+await banner("Si ejecutas esta linea\n console.log(__dirname);\n Ejecutara lo siguiente: ","Jarvis: ",1000);
+console.log(__dirname);
+await banner("Puedes observar el directorio donde estes xD","Jarvis: ",1000);
+
+await banner("Si ejecutas esta linea\n console.log(__filename);\n Ejecutara lo siguiente: ","Jarvis: ",1000);
+console.log(__filename);
+await banner("Puedes observar el nombre del archivo xD","Jarvis: ",1000);
+
+await banner("Si ejecutas esta linea\n console.log(setImmediate);\n Ejecutara lo siguiente: ","Jarvis: ",1000);
+console.log(setImmediate);
+await banner("Puedes observar propiedades del modulo setImmediate xD","Jarvis: ",1000);
+
+await banner("Si ejecutas esta linea tenemos un periodico \n console.log(process);\n Ejecutara lo siguiente: ","Jarvis: ",1900);
+await codigo(process,2000)
+await banner("Puedes observar propiedades del modulo process que SON BASTANTES","Jarvis: ",1900);
+
+await banner("Si puedes no usar variables globales aqui te presento como deberias declararlas para evitar problemas\n global.miVariable = 'elValor';\n y si luego le haces el llamado de esta forma\n console.log(miVariable);\n verias lo siguiente: ","Jarvis: ",1000);
+global.miVariable = 'elValor';
+await codigo(miVariable,1000);
+await banner("Puedes observar el valor de miVariable","Jarvis: ",1000);
+
+// await codigo("HOLA")
+console.log(`Terminando proceso... ADIOS!! vuelva pronto SIR, ${nombre}, ${nombre2}`)
 }
 main();
 
 
 /* let i = 0;
-let intervalo = setinterval(function () {
+let intervalo = setInterval(function () {
     console.log("hola mundoooooo");
     i++
     if (i === 3) {
-        clearinterval(intervalo);
+        clearInterval(intervalo);
     }
 }, 100);
 
