480ab0aae5f3a61a21b408e442986d869edded12
RodrigoMoreno <rroderickk@gmail.com>
2021-12-02 10:50:06 -0500

    Readme.md para 10x con vimVSC y globales.js para explicar la clase

--- modulos/globales.js
+++ modulos/globales.js
@@ -7,6 +7,7 @@ return new Promise(function (resolve, reject) {
     }, 30);
 });
 }
+
 async function banner(nombre,Jarvis) {
 return new Promise(function (resolve, reject) {
     setTimeout(function () {
@@ -22,7 +23,7 @@ return new Promise(function (resolve, reject) {
 async function codigo(nombre) {
     return new Promise(function (resolve, reject) {
         setTimeout(function () {
-            console.log(`${nombre}`);
+            console.log(nombre);
             resolve();
         }, 60);
     });
@@ -38,24 +39,44 @@ async function rodri(nombreRodri,pregunta) {
     });
 }
 
+async function ejemplo1(nombre) {
+    return new Promise(function (resolve, reject) {
+        setTimeout(function () {
+        let i = 0;
+        let intervalo = setInterval(function () {
+            console.log("hola mundoooooo");
+            i++
+            if (i === 3) {
+                clearInterval(intervalo);
+            }
+        }, 1000); 
+        console.log(nombre);
+                    resolve();
+                }, 1000);
+            });
+}
 
 let nombre = await banner("Rodri","Jarvis: Wellcome Sir");
 let nombre2 = await nombreP("Platzi");
 await banner("Platzi","Jarvis: Wellcome Sir");
-await banner("Esto es un codigo que puedes usar","Jarvis: ");
-await codigo("Puedo meter EESTO ES UN PEDAZO DE CODIGO")
-await rodri("R","para que sirve tal cosA? ")
-console.log(`Terminando proceso ${nombre}, ${nombre2}`)
+await banner("Esto es un codigo que puedes usar ","Jarvis: ");
+await codigo("ESTO ES UN PEDAZO DE CODIGO:\n let i = 0;  \n let intervalo = setinterval(function () {\n\t console.log(hola mundoooooo);\n\t i++ if (i === 3) {\n\t clearinterval(intervalo);\n\t }\n }, 100); ");
+await rodri("R","y para que sirve tal cosAAAA? ")
+await ejemplo1("Jarvis: Imprimira esta secuencia y cortara al imprimirlo i veces checkea: ");
+
+
+// console.log(`Terminando proceso ${nombre}, ${nombre2}`)
+
 }
 main();
 
 
 /* let i = 0;
-let intervalo = setInterval(function () {
-    console.log("Hola Mundoooooo");
+let intervalo = setinterval(function () {
+    console.log("hola mundoooooo");
     i++
     if (i === 3) {
-        clearInterval(intervalo);
+        clearinterval(intervalo);
     }
 }, 100);
 
